576c1aca89196761bcdab999b6fb22e8d8d2dbd2
karyssa-dandrea <karyssadandrea@gmail.com>
2021-08-27 15:20:02 -0700

    added const for noun and adjective

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // import functions
+const nounOneInput = document.getElementById('noun-one-input');
+const nounTwoInput = document.getElementById('noun-two-input');
+const nounThreeInput = document.getElementById('noun-three-input');
+const nounFourInput = document.getElementById('noun-four-input');
+const nounFiveInput = document.getElementById('noun-five-input');
+const nounSixInput = document.getElementById('noun-six-input');
+const nounSevenInput = document.getElementById('noun-seven-input');
+const nounEightInput = document.getElementById('noun-eight-input');
+
+const adjectiveOneInput = document.getElementById('adjective-one-input');
 
 // reference needed DOM elements
 
